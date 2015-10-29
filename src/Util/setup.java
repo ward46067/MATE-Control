2bb16667818cca0eef0927e9e1f1c0c088a7c14d
ward46067 <ward46067@sas.edu.sg>
2015-10-29 22:12:07 +0800

    cleaned formatting

--- src/Util/setup.java
+++ src/Util/setup.java
@@ -2,6 +2,7 @@ package Util;
 
 import GUI.GUI;
 import GUI.paint;
+import Math.math;
 import static Util.var.*;
 import java.awt.Graphics;
 import java.io.FileNotFoundException;
@@ -13,11 +14,14 @@ public class setup {
     public static void setup() throws FileNotFoundException, UnsupportedEncodingException{
         log.createLog();
         log.debuglog("Created logs");
+        
         color.colorRead();
         log.debuglog("Loaded XML");
+        
         color.colorSet();
         log.debuglog("Loaded Colors");
-        GUI.GUI();
         
+        GUI.GUI();
+        log.debuglog("Loaded GUI");
     }
 }
